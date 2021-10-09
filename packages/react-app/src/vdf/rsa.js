9ce67722a0d484522823508f5548f3eecd55cd26
calvbore <calv.bore@mail.com>
2021-10-08 23:30:46 -0600

    add origin ts code as comments to vdf implementation files

--- packages/react-app/src/vdf/rsa.js
+++ packages/react-app/src/vdf/rsa.js
@@ -57,3 +57,63 @@ function exp(a, e, modulus = exports.RSA_MODULUS) {
     return bignumber_1.BigNumber.from(resBN.toString());
 }
 exports.exp = exp;
+
+// rsa.ts https://github.com/kilic/evmvdf/blob/master/test/rsa.ts
+//
+// import { BigNumber } from '@ethersproject/bignumber';
+// import { hexlify, zeroPad } from '@ethersproject/bytes';
+// import { randomBytes } from '@ethersproject/random';
+// import BN from 'bn.js';
+//
+// export type E = BigNumber;
+//
+// const RSA_MODULUS_STR =
+//   '25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357';
+// export const RSA_MODULUS = BigNumber.from(RSA_MODULUS_STR);
+// export const RSA_MODULUS_BYTES = to256Bytes(RSA_MODULUS);
+//
+// export function randE(n: number = 256): E {
+//   return BigNumber.from(randomBytes(n)).mod(RSA_MODULUS);
+// }
+//
+// export function randBigNumber(n: number): E {
+//   return BigNumber.from(randomBytes(32));
+// }
+//
+// export function newE(n: string | number = 0): E {
+//   return BigNumber.from(n);
+// }
+//
+// export function to256Bytes(input: E): string {
+//   return hexlify(zeroPad(input.toHexString(), 256));
+// }
+//
+// export function to512Bytes(input: E): string {
+//   return hexlify(zeroPad(input.toHexString(), 512));
+// }
+//
+// export function toBytes(input: E, n: number): string {
+//   return hexlify(zeroPad(input.toHexString(), n));
+// }
+//
+// export function mulE(a: E, b: E, modulus: E = RSA_MODULUS): E {
+//   return a.mul(b).mod(modulus);
+// }
+//
+// export function find_q(a: E, b: E, modulus: E = RSA_MODULUS): E {
+//   return a.mul(b).div(modulus);
+// }
+//
+// export function addE(a: E, b: E, modulus: E = RSA_MODULUS): E {
+//   return a.add(b).mod(modulus);
+// }
+//
+// export function exp(a: E, e: E, modulus: E = RSA_MODULUS): E {
+//   const ctx = BN.red(new BN(modulus.toString()));
+//   const aBN = new BN(a.toString());
+//   const eBN = new BN(e.toString());
+//   let redBN = aBN.toRed(ctx);
+//   redBN = redBN.redPow(eBN);
+//   const resBN = redBN.fromRed();
+//   return BigNumber.from(resBN.toString());
+// }
--- packages/react-app/src/vdf/vdf.js
+++ packages/react-app/src/vdf/vdf.js
@@ -114,3 +114,140 @@ exports.verify = verify;
 // const t = 4;
 // const proof = evaluate(g, t);
 // console.log('must verify', verify(g, t, proof));
+
+// vdf.ts https://github.com/kilic/evmvdf/blob/master/test/vdf.ts
+//
+// import { ethers } from 'hardhat';
+//
+// import { E, exp, find_q, mulE, newE, randE } from './rsa';
+//
+// const MILLER_RABIN_ROUNDS = 15;
+// const DST = '0x';
+//
+// export interface Challenge {
+//   l: E;
+//   nonce: number;
+// }
+//
+// export interface Proof {
+//   challenge: Challenge;
+//   pi: E;
+//   y: E;
+//   q: E;
+// }
+//
+// export function hash(g: E, y: E, nonce: number, dst: string): E {
+//   return newE(ethers.utils.solidityKeccak256(['bytes', 'bytes', 'bytes', 'uint256'], [dst, g, y, nonce]));
+// }
+//
+// export function hashToPrime(g: E, y: E, dst: string): Challenge | null {
+//   for (let i = 0; i < 1 << 16; i++) {
+//     let candidate = hash(g, y, i, dst);
+//     if (candidate.and(1).eq(0)) {
+//       candidate = candidate.add(1);
+//     }
+//     if (isProbablePrime(candidate)) {
+//       return { l: candidate, nonce: i };
+//     }
+//   }
+//   return null;
+// }
+//
+// export function randPrime() {
+//   let i = 0;
+//   let candidate = randE(32);
+//   if (candidate.and(1).eq(0)) {
+//     candidate = candidate.add(1);
+//   }
+//   while (true) {
+//     if (isProbablePrime(candidate)) {
+//       return candidate;
+//     }
+//     candidate = candidate.add(2);
+//   }
+// }
+//
+// export function isProbablePrime(n: E): boolean {
+//   if (n.lt(3)) {
+//     return false;
+//   }
+//   if (!n.and(1).eq(1)) {
+//     return false;
+//   }
+//   let d = n.sub(1);
+//   let r = 0;
+//   while (d.and(1).eq(0)) {
+//     d = d.shr(1);
+//     r += 1;
+//   }
+//
+//   for (let i = 0; i < MILLER_RABIN_ROUNDS; i++) {
+//     let a = newE(10); //randBelow(n.sub(3)).add(2);
+//     let x = exp(a, d, n);
+//     if (x.eq(1) || x.eq(n.sub(1))) {
+//       continue;
+//     }
+//     let passed = false;
+//     for (let j = 1; j < r; j++) {
+//       x = x.mul(x).mod(n);
+//       if (x.eq(n.sub(1))) {
+//         passed = true;
+//         break;
+//       }
+//     }
+//     if (!passed) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// }
+//
+// export function evaluate(g: E, t: number): Proof {
+//   const e = newE(1).shl(t);
+//   const y = exp(g, e);
+//   const challenge = hashToPrime(g, y, DST)!;
+//
+//   let z = e;
+//   let r = newE(1);
+//   let pi = newE(1);
+//
+//   while (!z.eq(0)) {
+//     const r2 = r.mul(2);
+//     const b = r2.div(challenge.l);
+//     r = r2.mod(challenge.l);
+//     const gb = exp(g, b);
+//     pi = mulE(pi, pi);
+//     pi = mulE(pi, gb);
+//     z = z.sub(1);
+//   }
+//
+//   // some extra work for helper value
+//   const u1 = exp(pi, challenge.l);
+//   const u2 = exp(g, e);
+//   const q = find_q(u1, u2);
+//   return { pi, challenge, y, q };
+// }
+//
+// export function verify(g: E, t: number, proof: Proof): boolean {
+//   let l = hash(g, proof.y, proof.challenge.nonce, DST)!;
+//   if (l.and(1).eq(0)) {
+//     l = l.add(1);
+//   }
+//   if (!l.eq(proof.challenge.l)) {
+//     return false;
+//   }
+//   if (!isProbablePrime(l)) {
+//     return false;
+//   }
+//
+//   const T = newE(1).shl(t);
+//   const u1 = exp(proof.pi, proof.challenge.l);
+//   const u2 = exp(g, T);
+//   return mulE(u1, u2).eq(proof.y);
+// }
+//
+// // const g = randE();
+// // const t = 4;
+// // const proof = evaluate(g, t);
+// // console.log('must verify', verify(g, t, proof));
