f5456a835c2729b1c4c5a439741d1bb20a5ff663
calvbore <calv.bore@mail.com>
2021-10-11 14:03:28 -0600

    import VerifyVDF.sol into YourContract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,10 +1,11 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "./VerifyVDF.sol";
 
-contract YourContract {
+contract YourContract is VerifyVDF {
 
   //event SetPurpose(address sender, string purpose);
 
