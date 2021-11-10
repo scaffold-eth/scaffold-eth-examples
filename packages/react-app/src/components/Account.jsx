a80238cbc828b4ed43fbc8f514b83a3b7e8a4404
Jaxcoder <codenamejason@outlook.com>
2021-11-10 14:22:54 -0500

    Initialize multi-sig-wallet

---
+++ packages/react-app/src/components/Account.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Button } from "antd";
+import Address from "./Address";
+import Balance from "./Balance";
+import Wallet from "./Wallet";
+
+export default function Account({
+  address,
+  userProvider,
+  localProvider,
+  mainnetProvider,
+  price,
+  minimized,
+  web3Modal,
+  loadWeb3Modal,
+  logoutOfWeb3Modal,
+  blockExplorer,
+}) {
+  const modalButtons = [];
+  if (web3Modal) {
+    if (web3Modal.cachedProvider) {
+      modalButtons.push(
+        <Button
+          key="logoutbutton"
+          style={{ verticalAlign: "top", marginLeft: 8, marginTop: 4 }}
+          shape="round"
+          size="large"
+          onClick={logoutOfWeb3Modal}
+        >
+          logout
+        </Button>,
+      );
+    } else {
+      modalButtons.push(
+        <Button
+          key="loginbutton"
+          style={{ verticalAlign: "top", marginLeft: 8, marginTop: 4 }}
+          shape="round"
+          size="large"
+          type={minimized ? "default" : "primary"}
+          onClick={loadWeb3Modal}
+        >
+          connect
+        </Button>,
+      );
+    }
+  }
+
+  const display = minimized ? (
+    ""
+  ) : (
+    <span>
+      {address ? <Address value={address} ensProvider={mainnetProvider} blockExplorer={blockExplorer} /> : "Connecting..."}
+      <Balance address={address} provider={localProvider} dollarMultiplier={price} />
+      <Wallet address={address} provider={userProvider} ensProvider={mainnetProvider} price={price} />
+    </span>
+  );
+
+  return (
+    <div>
+      {display}
+      {modalButtons}
+    </div>
+  );
+}
---
+++ packages/react-app/src/constants.js
@@ -0,0 +1,5 @@
+// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
+export const INFURA_ID = "5ce0898319eb4f5c9d4c982c8f78392a";
+
+//MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
