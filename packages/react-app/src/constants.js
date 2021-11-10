e6c49c093cbfcbb5b60fad6988686caf4743edaf
Jaxcoder <codenamejason@outlook.com>
2021-11-10 14:24:56 -0500

    Initialize notation-nft

---
+++ packages/react-app/src/constants.js
@@ -0,0 +1,447 @@
+// MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
+export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+
+// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+
+// BLOCKNATIVE ID FOR Notify.js:
+export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
+
+// EXTERNAL CONTRACTS
+
+export const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+export const SONGDATA = `X:1
+T:This Song Will Never Die
+L:1/4
+Q:1/4=165
+M:4/4
+I:linebreak $
+K:Bb
+V:1 treble nm="Voice"
+V:2 bass transpose=-12 nm="Bass Guitar" snm="B. Guit."
+V:1
+"^Swing" B,3/2 C/- C C | B,/ B, G,/- G, z | B,3/2 C/- C C | D/ D F/- F z |$ G3/2 F/- F .B |
+w: This song * will|ne- ver die *|this song * will|ne- ver die *|this song * will|
+B/ B B/- B B,/B,/ | B, B, C C/-C/ | C B, z2 ||$ z2 z G,/F,/ | B, z z2 | z4 || B/ B B/- B z |
+w: ne- ver die * til the|heat death of the un-|i- verse|oh _|_||here i am *|
+A/ B B/- B z |$ G/ A B/- B/B/c/c/ | c B B D/D/ || D C/C/- C z G,/ | G,/ C B,/- B, z |$
+w: here i am *|here i am _ is what i|want to say what i|want to say _ is|i was here _|
+D/ D C/- C G, | G,/ B, B,/- B, z | D F/F/ F F | G/G/ G G F |$
+w: full of an- * xie-|ty and fear _|feel kind of weird a|lit- lie bit funk- y|
+[K:treble-8 m=B,] B,/B,/B,/B,/ B,/B,/B,/B,/ | C C C F, | (G, F,) z2 z || B,,3/2 C,/- C, z .G,,/ |$
+w: feel- ing like an ov- er- e- du-|cat- ed- mon- key|_ _|this song _ will|
+[K:treble] B,/ B, G,/- G, z | B,3/2 C/- C!mp! C | D/ D F/- F z | G3/2 F/- F!mp! B |$
+w: ne- ver die _|this song _ will|ne- ver die _|this song _ will|
+B/ B B/- B .B,/B,/ | B, B, C3/2 C/ | C/C/ B, z2 || z2 z G, |$ B, z z2 | z2 z G, ||
+w: ne- ver die _ til the|heat death of the|u- ni- verse|oh|_|oh|
+!p! B,/ B, C/- C z | D/ D F/- F F/(G/ |$ G/) F3/2- F2 | z7/2 |!p! B,/ B, C/- C z |
+w: you could be _|hear- ing this _ to- mo-|* rrow _||you could be _|
+D/ D F/- F/ F (F/ |$ F) F/G/- G c | B2 B/ c/!mf!G/ | B/ G .B/- B B/G/- | G/ F3/2 z B |$
+w: hear- ing this _ one thous-|* sand years _ from|now but if you're|lis- ten- ing _ to- mo-|* row or|
+[K:treble-8 m=B,] B, B, C3/2 C/- | C C z/ B, z | F, F, z/ B,, B,, | B,, B,,/B,,/- B,, B,,/B,,/ |$
+w: in some far flung|_ fut- ture|i want you to|sing a- long _ and i|
+C, C, C, C, |$[K:treble] F2 z2 || B,3/2 C/- C!mp! C | B,/ B, G,/- G, z | B,3/2 C/- C!mp! C |
+w: think that you know|how|this song * will|ne- ver die *|this song _ will|
+D/ D F/- F z |$ G3/2 F/- F .B | B/ B B/- B .B,/B,/ | C B, C3/2 C/ | C/B,/ B, z2 ||$
+w: ne- ver die *|this song * will|ne- ver die * til the|heat death of the|u- ni- verse|
+[K:treble-8 m=B,] z2 z G,, | B,, B,, z2 | z4 || B,3/2 G,/- G, z B,/ | B, A, F, G, |$
+w: oh|_ _||some day _ the|world will be con-|
+[K:treble] d2 c c/(B/ | G) z z2 z/ | B3/2 G/- G z | B/ B B/-!mp! B/ G (d/ |$ d/) d d/- d/ c B/ |
+w: sumed by the sun|_|some day _|you and me _ will die|_ just like _ ev- ery-|
+B z z2 | c3/2 c/- c d | c/ B G/ z z C/ |$[K:treble-8 m=B,] C/ C C/ C/ D3/2 | C z z/ z z3/2 B,/ |
+w: one|Mean- while _ we|do our best to|be kind and to have|fun the|
+B, B, B, G, | .B,/ .B, B,/- B,/ B, B,/ |$ C/C/ C G, F, | F,2 F, z || B,,3/2 C,/- C, z .G,,/ |
+w: end is the be-|gin- ing and * the be|gin- ning has just be|gun oh|this song * will|
+B,,/ B,, G,,/- G,, z |$[K:treble] B,3/2 C/- C .C | D/ D F/- F z | G3/2 F/- F!mp! B |
+w: ne- ver die *|this song * will|ne- ver die *|this song * will|
+B/ B B/- B .B,/B,/ |$ B, B, C3/2 C/ | C/B,/ B, z .G,/G,/ || G, B, C3/2 C/ | C/B,/ B, z .G,/G,/ |$
+w: ne- ver die * til the|heat death of the|un- i- verse ti the|heat death of the|un- i- verse til the|
+B, B, C3/2 C/ | C/B,/ B, z F,/F,/ | C C C3/2 C/ |$ C/B,/ B, z F,/F,/ | F, B, C3/2 C/ |
+w: heat death of the|un- i- verse til the|heat death of the|uni- i- verse till the|heat death of the|
+C/B,/ B, z2 |]
+w: un- i- verse|
+V:2
+B,, F,/- z/ F, z | B,, G,,/- z/ G,, z | B,, F,/- z/ F, z | B,, D,/- z/ D, z |$ B,, F,/- z/ F, z |
+G, E,/- z/ E, z | E, z F, z | B,, B,, B,, F,,/B,,/ ||$ z .B,, .B,, F,, | B,, B,, B,, B,,/ z/ |
+z4 || .B,, z z2 | .F, z z2 |$ .G, z z2 | E, E,/E,/ E, z || B,, z/ F,/- F, z z/ |
+G, F,/E,/- E, A,, |$ B,, F,/- z/ F, z | G, E,/- z/ E, z | B,, B,,2 B,, | C,/C,/ C,2 z |$
+E, E, E, z | F, F, F, F, | F, F, F,2 z || B,, F,/- z/ F, z z/ |$ B,, G,,/- z/ G,, z |
+B,, F,/- z/ F, z | B,, D,/- z/ D, z | B,, F,/- z/ F, z |$ G, E,/- z/ E, E, | E, z F, z |
+B,, B,, B,, B,,/ z/ || z .B,, .B,, G,, |$ B,, B,, B,, F,,/B,,/ | z4 || G, F,/- z/ F, z |
+G, E,/- z/ E, z |$ B,, F,/- F, z z/ | G, E,/- E, A,, | B,, F,/- z/ F, z | G, E,/- z/ E, z |$
+B, z/ F,/- F, z | G, E,/- E, z | B,, F,/- z/ F, z | G, E,/- E, z z/ |$ B,, z F, z |
+G, z E,/- E, z | B,, z F,/- F, z | G, z/ E,/- E, z |$ C, C, C, E |$ F, F, F, F,, ||
+B,, F,/- z/ F, z | B,, G,,/- z/ G,, z | B,, F,/- z/ F, z | B,, D,/- z/ D, z |$ B,, F,/- z/ F, z |
+G, E,/- z/ E, E, | E, z F, z | B,, B,, B,, z/ B,,/ ||$ z B,, B,, G,, | B,, B,, B,, B,,/ z/ | z4 ||
+E, E, E, E, z/ | F, F, F, .F, |$ G,3/2 E,/ F, B,, | E, E, E,/E,/E,/ E, | E, E, E, E, |
+F, F,2 F, |$ G,2 F, D, | E, E,/E,/ E,/E,/ E, | C, C,/C,/ C, .C, | E, E, E, E, z/ |$ C, C, C, A,, |
+E, E,/E,/E,/ E, E,/ E, z/ | B,, z F, z | G, E,/- z/ E,/E,/ .E, |$ F,/F,/F,/F,/ F,/F,/ F, |
+F,2 z2 || B,, F,/- z/ F, z z/ | B,, G,,/- z/ G,, z |$ B,, F,/- z/ F, z | B,, D,/- z/ D, z |
+B,, F,/- z/ F, z | G, E,/- z/ E, z |$ E, z F, z | B,, B,, B,, z || E, E, F, B,,/ z/ |
+B,, B,, B,, z |$ E, E, F, B,,/ z/ | B,, B,, z F,, | E, z F, z |$ B,, B,, B,, F,,/B,,/ |
+E, z F, z | B,, B,, B,, z |]`
+
+export const DAI_ABI = [
+  {
+    inputs: [{ internalType: "uint256", name: "chainId_", type: "uint256" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "guy", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: true,
+    inputs: [
+      { indexed: true, internalType: "bytes4", name: "sig", type: "bytes4" },
+      { indexed: true, internalType: "address", name: "usr", type: "address" },
+      { indexed: true, internalType: "bytes32", name: "arg1", type: "bytes32" },
+      { indexed: true, internalType: "bytes32", name: "arg2", type: "bytes32" },
+      { indexed: false, internalType: "bytes", name: "data", type: "bytes" },
+    ],
+    name: "LogNote",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "dst", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "DOMAIN_SEPARATOR",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "PERMIT_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "burn",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "deny",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "mint",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "move",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "nonces",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "holder", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "nonce", type: "uint256" },
+      { internalType: "uint256", name: "expiry", type: "uint256" },
+      { internalType: "bool", name: "allowed", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "permit",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "pull",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "push",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "rely",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "wards",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const NETWORK = chainId => {
+  for (const n in NETWORKS) {
+    if (NETWORKS[n].chainId === chainId) {
+      return NETWORKS[n];
+    }
+  }
+};
+
+export const NETWORKS = {
+  localhost: {
+    name: "localhost",
+    color: "#666666",
+    chainId: 31337,
+    blockExplorer: "",
+    rpcUrl: "http://" + window.location.hostname + ":8545",
+  },
+  mainnet: {
+    name: "mainnet",
+    color: "#ff8b9e",
+    chainId: 1,
+    rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+    blockExplorer: "https://etherscan.io/",
+  },
+  kovan: {
+    name: "kovan",
+    color: "#7003DD",
+    chainId: 42,
+    rpcUrl: `https://kovan.infura.io/v3/${INFURA_ID}`,
+    blockExplorer: "https://kovan.etherscan.io/",
+    faucet: "https://gitter.im/kovan-testnet/faucet", // https://faucet.kovan.network/
+  },
+  rinkeby: {
+    name: "rinkeby",
+    color: "#e0d068",
+    chainId: 4,
+    rpcUrl: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
+    faucet: "https://faucet.rinkeby.io/",
+    blockExplorer: "https://rinkeby.etherscan.io/",
+  },
+  ropsten: {
+    name: "ropsten",
+    color: "#F60D09",
+    chainId: 3,
+    faucet: "https://faucet.ropsten.be/",
+    blockExplorer: "https://ropsten.etherscan.io/",
+    rpcUrl: `https://ropsten.infura.io/v3/${INFURA_ID}`,
+  },
+  goerli: {
+    name: "goerli",
+    color: "#0975F6",
+    chainId: 5,
+    faucet: "https://goerli-faucet.slock.it/",
+    blockExplorer: "https://goerli.etherscan.io/",
+    rpcUrl: `https://goerli.infura.io/v3/${INFURA_ID}`,
+  },
+  xdai: {
+    name: "xdai",
+    color: "#48a9a6",
+    chainId: 100,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://dai.poa.network",
+    faucet: "https://xdai-faucet.top/",
+    blockExplorer: "https://blockscout.com/poa/xdai/",
+  },
+  matic: {
+    name: "matic",
+    color: "#2bbdf7",
+    chainId: 137,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://rpc-mainnet.maticvigil.com",
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://explorer-mainnet.maticvigil.com//",
+  },
+  mumbai: {
+    name: "mumbai",
+    color: "#92D9FA",
+    chainId: 80001,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://rpc-mumbai.maticvigil.com",
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://mumbai-explorer.matic.today/",
+  },
+};
