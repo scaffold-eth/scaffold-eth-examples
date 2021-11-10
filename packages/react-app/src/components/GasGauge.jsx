068f2247ae5d8f819e93acda457c1abb13942be2
Jaxcoder <codenamejason@outlook.com>
2021-11-10 14:22:51 -0500

    Initialize guild-and-spellbook

---
+++ packages/react-app/src/components/GasGauge.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "antd";
+
+export default function GasGauge(props) {
+  return (
+    <Button
+      onClick={() => {
+        window.open("https://ethgasstation.info/");
+      }}
+      size="large"
+      shape="round"
+    >
+      <span style={{ marginRight: 8 }}>⛽️</span>
+      {parseInt(props.gasPrice, 10) / 10 ** 9}g
+    </Button>
+  );
+}
