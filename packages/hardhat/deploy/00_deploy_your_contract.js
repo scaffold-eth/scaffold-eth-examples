af6e68cd614bceeadc3998f4772cc2e22149bfda
Jaxcoder <codenamejason@outlook.com>
2021-11-10 14:27:06 -0500

    Initialize gtc-staker

---
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -0,0 +1,55 @@
+// deploy/00_deploy_your_contract.js
+
+const { ethers } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  //! prod GTC address
+  let GTC = { address: "0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F" };
+
+  // deploy mock token contract ~ if not on mainnet
+  if (chainId !== "1") {
+    // deploy mock GTC
+    GTC = await deploy("GTC", {
+      from: deployer,
+<<<<<<< HEAD
+      args: ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"],
+=======
+      //front-end address vvvvvvvvvvvvvvv replace with args: [admins[0]], for deploy
+      args: ["0x43aa5A6a8CB1Fec575583b7531b57bd79067309E"], 
+>>>>>>> 98839b1c983fff774798c56598d89d3682705f95
+      log: true,
+    });
+  }
+
+  // deploy Staking Contract ~ any network
+  await deploy("StakingGTC", {
+    from: deployer,
+    args: ["0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F"],
+    log: true,
+  });
+
+  // Getting a previously deployed contract
+  const StakeGTCContract = await ethers.getContract("StakingGTC", deployer);
+
+  // log the GTC and StreamFactory addresses
+  console.log({
+    GTC: GTC.address,
+    streamFactory: StakeGTCContract.address,
+  });
+
+  // make sure were not on the local chain...
+  if (chainId !== "31337") {
+    // verigy the staking contract
+    await run("verify:verify", {
+      address: StakeGTCContract.address,
+      contract: "contracts/StakingGTC.sol:StakingGTC",
+      constructorArguments: [GTC.address],
+    });
+  }
+};
+
+module.exports.tags = ["GTC", "StakingGTC"];
