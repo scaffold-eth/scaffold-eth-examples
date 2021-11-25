852e6f5db05787d23db913ececc2a5a2fe1d0bb9
calvbore <calv.bore@mail.com>
2021-11-24 22:04:50 -0600

    add custom key and value inputs to smt interface

--- packages/react-app/src/components/SmtInterface.jsx
+++ packages/react-app/src/components/SmtInterface.jsx
@@ -4,17 +4,51 @@ import ReactJson from 'react-json-view';
 import { useSMT } from "../hooks";
 
 export default function SmtInterface() {
-  const tree = useSMT([1, 1]);
+
+  const [treeLeaves, setTreeLeaves] = useState({});
+
+  const tree = useSMT(treeLeaves);
+
+  const [insertKey, setinsertkey] = useState();
+  const [insertValue, setInsertValue] = useState();
 
   // const proof = tree.createProof(BigInt(0));
   // console.log("smt proof: ", proof);
 
-  if (tree.db) console.log(tree.db.nodes);
+
+  async function insertToTree() {
+    const res = await tree.insert(insertKey, insertValue);
+    console.log(res);
+
+    const treeLeafKeys = Object.keys(treeLeaves);
+    let newTreeLeaves= {}
+    for (let i = 0; i < treeLeafKeys.length; i++) {
+      newTreeLeaves[treeLeafKeys[i]] = treeLeaves[treeLeafKeys[i]];
+    }
+    newTreeLeaves[insertKey] = insertValue;
+    setTreeLeaves(newTreeLeaves);
+  }
 
   return (
     <div>
       <p>{tree.root ? tree.root.toString() : "undefined"}</p>
       {/*<p>{tree.oldRoot ? tree.oldRoot.toString() : "undefined"}</p>*/}
+      <span>
+        <InputNumber
+          onChange={(n) => setinsertkey(n)}
+        />
+      </span>
+      <span>
+        <InputNumber
+          onChange={(n) => setInsertValue(n)}
+        />
+      </span>
+      <Button
+        onClick={insertToTree}
+        type="primary"
+      >
+        Insert
+      </Button>
     </div>
   );
 }
