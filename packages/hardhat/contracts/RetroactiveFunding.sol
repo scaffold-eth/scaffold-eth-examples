6964233431227217d42c229377bf1eb26d45eaed
viraj124 <virajm72@gmail.com>
2021-12-01 10:08:50 +0530

    refactor: add logic to check for existing token/eth pool

--- packages/hardhat/contracts/RetroactiveFunding.sol
+++ packages/hardhat/contracts/RetroactiveFunding.sol
@@ -1,19 +1,31 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
 
 contract RetroactiveFunding is ERC20 {
-    uint public constant mintAmount = 1 ether;
+    uint public constant mintAmount = 10000 ether;
+    address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    // 1% fee
+    uint24 public constant fee = 10000;
+    IUniswapV3Factory public immutable factory;
 
-    constructor(string memory name, string memory symbol, uint share) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, uint share, IUniswapV3Factory _factory) ERC20(name, symbol) {
         // safe math already supported
         uint amountToMint = mintAmount - (mintAmount* share / 100);
+        // mint apt. amount based on share percentage to the project owner and contract
         _mint(msg.sender, amountToMint);
+        // funds to be used for adding liquidity
         _mint(address(this), mintAmount - amountToMint);
+        factory = _factory;
     }
 
     function fundProject() external payable {
-        // uniswap code to first check if the address(this)/eth pool exists and id not create it and add eth liquidity and the erc20 token liquidity stored in the contract, if yes add only eth liquidity to the pool
+        address pool = factory.getPool(address(this), weth, fee);
+        if (pool == address(0)) {
+            // create new pool
+        }
+        // add eth liquidity from whale and token liquidity from contract
     }
 
     function sellToken() external {
