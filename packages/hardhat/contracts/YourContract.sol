383c64bcbbb3eced7e793ca7bb3e6fed37d38cc4
Calvin Quin Boehr <calv.bore@gmail.com>
2021-12-02 00:15:16 -0600

    use default generated contract in yourcontract.sol

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,10 +1,13 @@
-pragma solidity >=0.6.11 < 0.9.0;
+pragma solidity 0.6.11;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-import "./Add2TreeVerifier.sol";
+import "./Verifier.sol";
+// import "./Add2TreeVerifier.sol";
+// import "./ProveInTreeVerifier.sol";
 
-contract YourContract is Verifier {
+contract YourContract {
+    using Verifier for *;
 
   //event SetPurpose(address sender, string purpose);
     event AddLeaf(
@@ -29,13 +32,42 @@ contract YourContract is Verifier {
     ) external {
         require(input[1] == root, "addLeaf: Invalid Root");
         require(input[2] == nextKey, "addLeaf: Invalid Key");
-        require(verifyProof(a, b, c, input) == true, "addLeaf: Invalid Proof");
+        require(verifyAdd2TreeProof(a, b, c, input) == true, "addLeaf: Invalid Proof");
 
         leafValues.push(input[3]);
         nextKey = leafValues.length;
         root = input[0];
 
         emit AddLeaf(input[2], input[3], input[1]);
+    }
+
+    function proveMembership(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[1] memory input
+    ) external view returns (bool r) {
+        require(input[0] == root, "proveMembership: Invalid Root");
+        r = verifyProveInTreeProof(a, b, c, input);
+        require(r == true, "proveMembership: Invalid Proof");
+    }
+
+    function verifyAdd2TreeProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[6] memory input
+    ) public view returns (bool) {
+        return Verifier.verifyAdd2TreeProof(a, b, c, input);
+    }
+
+    function verifyProveInTreeProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[1] memory input
+    ) public view returns (bool) {
+        return Verifier.verifyProveInTreeProof(a, b, c, input);
     }
 
 }
