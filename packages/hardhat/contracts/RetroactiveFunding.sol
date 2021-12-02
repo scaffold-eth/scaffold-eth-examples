6d4e15fd3da970860a0556350d8403c36aa3fc25
viraj124 <virajm72@gmail.com>
2021-12-02 09:27:02 +0530

    feat: added pool creation and initialization code

--- packages/hardhat/contracts/RetroactiveFunding.sol
+++ packages/hardhat/contracts/RetroactiveFunding.sol
@@ -1,30 +1,51 @@
-pragma solidity ^0.8.7;
+pragma solidity =0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 
 contract RetroactiveFunding is ERC20 {
-    uint public constant mintAmount = 10000 ether;
+    uint public constant mintAmount = 1000 ether;
     address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     // 1% fee
     uint24 public constant fee = 10000;
+    uint160 public constant initialPrice = 64;
     IUniswapV3Factory public immutable factory;
+    INonfungiblePositionManager public immutable positionManager;
 
-    constructor(string memory name, string memory symbol, uint share, IUniswapV3Factory _factory) ERC20(name, symbol) {
-        // safe math already supported
-        uint amountToMint = mintAmount - (mintAmount* share / 100);
-        // mint apt. amount based on share percentage to the project owner and contract
-        _mint(msg.sender, amountToMint);
-        // funds to be used for adding liquidity
-        _mint(address(this), mintAmount - amountToMint);
+    constructor(string memory name, string memory symbol, IUniswapV3Factory _factory, INonfungiblePositionManager _positionManager) ERC20(name, symbol) {
+        _mint(msg.sender, mintAmount);
         factory = _factory;
+        positionManager = _positionManager;
     }
 
+    function getPool() public view returns(address) {
+        return factory.getPool(address(this), weth, fee);
+    }
+
+    function sqrt(uint160 x) internal returns (uint160 y) {
+    uint z = (x + 1) / 2;
+    y = x;
+    while (z < y) {
+        y = z;
+        z = (x / z + z) / 2;
+    }
+}
+
     function fundProject() external payable {
-        address pool = factory.getPool(address(this), weth, fee);
+        address pool = getPool();
         if (pool == address(0)) {
             // create new pool
+            pool = factory.createPool(address(this), weth, fee);
+            // initialize pool
+            uint160 sqrtPriceX96 = (sqrt(initialPrice) * 2) ** 96;
+            IUniswapV3Pool(pool).initialize(sqrtPriceX96);
         }
+        _mint(address(this), msg.value);
+        IERC20(address(this)).approve(address(positionManager), msg.value);
         // add eth liquidity from whale and token liquidity from contract
     }
 
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -278,8 +278,17 @@ module.exports = {
   },
   solidity: {
     compilers: [
+      // {
+      //   version: "0.8.7",
+      //   settings: {
+      //     optimizer: {
+      //       enabled: true,
+      //       runs: 200,
+      //     },
+      //   },
+      // },
       {
-        version: "0.8.7",
+        version: "0.7.6",
         settings: {
           optimizer: {
             enabled: true,
