b7f701f4242cc3c239ccb938a2670c182ebb4b6e
viraj124 <virajm72@gmail.com>
2021-12-05 10:01:38 +0530

    feat: add swap code

--- packages/hardhat/contracts/RetroactiveFunding.sol
+++ packages/hardhat/contracts/RetroactiveFunding.sol
@@ -4,34 +4,46 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-// import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import "./interfaces/INonfungiblePositionManager.sol";
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract RetroactiveFunding is ERC20 {
-    uint256 public constant mintAmount = 1000 ether;
+    uint256 public constant mintAmount = 100 ether;
     uint256 public tokenId;
     address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     // 1% fee
     uint24 public constant fee = 10000;
+    // initial price used to initialize pool is project token / eth
     uint160 public constant initialPrice = 64;
     IUniswapV3Factory public immutable factory;
     INonfungiblePositionManager public immutable positionManager;
+    ISwapRouter public immutable swapRouter;
 
     constructor(
         string memory name,
         string memory symbol,
         IUniswapV3Factory _factory,
-        INonfungiblePositionManager _positionManager
+        INonfungiblePositionManager _positionManager,
+        ISwapRouter _swapRouter
     ) ERC20(name, symbol) {
         _mint(msg.sender, mintAmount);
         factory = _factory;
         positionManager = _positionManager;
+        swapRouter = _swapRouter;
     }
 
+    /**
+     * @notice Gets pool address
+     */
     function getPool() public view returns (address) {
         return factory.getPool(address(this), weth, fee);
     }
 
+    /**
+     * @notice Gets sq root of a number
+     * @param x no to get the sq root of
+     */
     function sqrt(uint160 x) internal pure returns (uint160 y) {
         uint160 z = (x + 1) / 2;
         y = x;
@@ -41,8 +53,12 @@ contract RetroactiveFunding is ERC20 {
         }
     }
 
+    /**
+     * @notice Called by a whale who adds liquidity, the same amount of lp tokens are minted to the contract & added tot he pool
+     */
     function fundProject() external payable {
         address pool = getPool();
+        // create & initialize the pool if it doesn not exist
         if (pool == address(0)) {
             // create new pool
             pool = factory.createPool(address(this), weth, fee);
@@ -51,8 +67,10 @@ contract RetroactiveFunding is ERC20 {
             IUniswapV3Pool(pool).initialize(sqrtPriceX96);
         }
         _mint(address(this), msg.value);
-        IERC20(address(this)).approve(address(positionManager), msg.value);
+        TransferHelper.safeApprove(address(this), address(positionManager),  msg.value);
+
         // add eth liquidity from whale and token liquidity from contract
+        // checks if the position has been minted, if not mint it else adds more liquidity
         if (positionManager.balanceOf(address(this)) > 0) {
             // increase liquidity
             INonfungiblePositionManager.IncreaseLiquidityParams memory params =
@@ -72,6 +90,7 @@ contract RetroactiveFunding is ERC20 {
                     token0: address(this),
                     token1: weth,
                     fee: fee,
+                    // full range of ticks is considered currently
                     tickLower: -887272,
                     tickUpper: 887272,
                     amount0Desired: msg.value,
@@ -86,8 +105,30 @@ contract RetroactiveFunding is ERC20 {
         }
     }
 
-    function sellToken() external {
+    /**
+     * @notice Called by token holder to swap token for eth 
+     */
+    function sellToken(uint _amount) external {
         // called by token holders to swap token for eth
         // uniswap router code needs to be added here
+        TransferHelper.safeTransferFrom(address(this), msg.sender, address(this), _amount);
+        // Approve the router to spend DAI.
+        TransferHelper.safeApprove(address(this), address(swapRouter), _amount);
+            ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(this),
+                tokenOut: weth,
+                fee: fee,
+                recipient: msg.sender,
+                deadline: block.timestamp + 1000,
+                amountIn: _amount,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        // The call to `exactInputSingle` executes the swap.
+        swapRouter.exactInputSingle(params);
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "sending eth failed");
     }
 }
